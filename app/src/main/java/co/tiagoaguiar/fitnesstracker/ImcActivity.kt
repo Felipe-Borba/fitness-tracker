09e577735e237e42fe622a372c87412baa5587b2
Felipe Borba <felipe.borba0109@gmail.com>
2024-01-11 19:45:08 -0300

    add back arrow in menu

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -69,6 +69,7 @@ class ImcActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_search) {
+            finish()
             openListActivity()
         }
         return super.onOptionsItemSelected(item)
